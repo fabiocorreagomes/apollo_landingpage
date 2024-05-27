e676257dae073de476f3c6d304e9e8be6ca2e0ce
Fabio Correa Gomes <fabiocorreagomes@gmail.com>
2024-05-27 16:48:56 -0300

    underline on a text

--- src/components/Container_13.tsx
+++ src/components/Container_13.tsx
@@ -3,7 +3,7 @@ export function Container13() {
         <div>
 
             <div className="text-black px-[55px] py-[66px] sm:px-[55px] md:px-[111px] lg:px-[222px] xl:px-[222px] items-center content-center text-center text-xs">
-            <p><strong>Este site não é afiliado ao Facebook ou a qualquer entidade do Facebook.</strong> Depois que você sair do Facebook, a responsabilidade não é deles e sim do nosso site. Fazemos todos os esforços para indicar claramente e mostrar todas as provas do produto e usamos resultados reais.</p>
+            <p><strong><u>Este site não é afiliado ao Facebook ou a qualquer entidade do Facebook.</u></strong> Depois que você sair do Facebook, a responsabilidade não é deles e sim do nosso site. Fazemos todos os esforços para indicar claramente e mostrar todas as provas do produto e usamos resultados reais.</p>
             </div>
 
         </div>
